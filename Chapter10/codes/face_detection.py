25a43c7433272bc7ff38c5a11ac613b2c1a155ba
shubham763 <60926838+shubham763@users.noreply.github.com>
2020-07-30 10:41:29 +0530

    Update face_detection.py

--- Chapter10/codes/face_detection.py
+++ Chapter10/codes/face_detection.py
@@ -12,7 +12,7 @@ faces = face_cascade.detectMultiScale(img_gray, 1.3, 5)
 
 for (x,y,w,h) in faces:
     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
+    roi_gray = img_gray[y:y+h, x:x+w]
     roi_color = img[y:y+h, x:x+w]
     eyes = eye_cascade.detectMultiScale(roi_gray)
     for (ex,ey,ew,eh) in eyes:
